c9680eee4fa279ef322675b923d590f78f472501
Thomas Pourchel <tpourchel@auchan.fr>
2023-06-19 14:40:37 +0200

    modifs

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000
 const route = require("./routes/routes");
 
-app.use("/blagues", route);
+app.use("/jokes", route);
 
 app.listen(port, () => {
   console.log(`Server running on ${port}`)
